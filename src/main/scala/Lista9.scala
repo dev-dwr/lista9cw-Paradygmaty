e82ab18b0210b10a7b7eb52980ea498ca2187bc5
dev-dwr <dawid.rymarczyk02@gmail.com>
2023-01-16 09:35:59 +0100

    time 3 change test

--- src/main/scala/Lista9.scala
+++ src/main/scala/Lista9.scala
@@ -6,4 +6,7 @@ object Lista9 extends App {
   new Vehicle("producer", "model", 1111,
     "number_registration") //main constructor
   new Vehicle("producer", "model", registrationNumber = "number-some")
+
+  val time3 = new Time3(21, 15);
+  println(time3.getMinute)
 }
